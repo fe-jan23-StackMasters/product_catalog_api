483a148ee4a0d56df7799ba8f1dc6a5c98557561
kirill-vypirovskyi <kirill.vypirovsky@gmail.com>
2023-05-08 20:11:08 +0300

    set up + data uploaded

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
-import express from "express";
+import express from 'express';
+import { initDb } from './initDb.js';
+
 const PORT = 3000;
 const app = express();
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World");
+const sequelize = initDb();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World');
 });
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
---
+++ src/initDb.ts
@@ -0,0 +1,21 @@
+import { Sequelize } from 'sequelize-typescript';
+import 'dotenv/config.js';
+
+const { DB_URL } = process.env;
+
+export const initDb = async () => {
+  const sequelize = new Sequelize(DB_URL, {
+    dialectOptions: {
+      ssl: true,
+    },
+  });
+
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+
+  return sequelize;
+};
